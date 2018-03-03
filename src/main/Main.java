e2e4afe617ff0fed20656e098a99072d18829f8c
40CompilerErrors <danvgdesign@gmail.com>
2018-03-03 23:09:40 +0100

    Add files via upload

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
+	
 	static int BusBin(int[] a, int x) {
 		if (a.length == 0 || a[0] > x || a[a.length - 1] < x) {
 			return 0;
@@ -84,35 +88,53 @@ public class Main {
 		int j = med + 1;
 		for (int cont = ini; i <= fin; i++) {
 			if (Liz[i] < Lde[j]) {
-				L[cont] = Liz[cont];
+				L[cont] = Liz[i];
 				i++;
 			} else {
-				L[cont] = Lde[cont];
+				L[cont] = Lde[j];
 				j++;
 			}
 		}
 		return start - Runtime.getRuntime().freeMemory();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int arraySize = 5000;
+		
+		FileWriter fileWriter = null;
+		fileWriter = new FileWriter("Eficiencia.csv");
+		
+		final String FILE_HEADER = "Quicksort Memoria,Quicksort nsec,Mergesort Memoria,Mergesort nsc,Binary Search nsec,Sequential Search nsec";
+		
 
 		for (; arraySize <= 15000; arraySize += 2500) {
+			
 			int iteracion = 100;
-			double Bintime = 0;
-			double mershtime = 0;
-			double quicktime = 0;
-			double Sectime = 0;
+			double binTimeTotal = 0;
+			double mergeTimeTotal = 0;
+			double quickTimeTotal = 0;
+			double secTimeTotal = 0;
 			int[] quick;
 			int[] mershe;
-			long memoriaQuick = 0;
-			long memoriaMersh = 0;
+			long memoriaQuickTotal = 0;
+			long memoriaMergeTotal = 0;
+			
+			fileWriter.append("Tamaño Array," + arraySize);
+			fileWriter.append('\n');
+			fileWriter.append(FILE_HEADER.toString());
+			fileWriter.append('\n');
+			
+			for (int i = 0; i < 5; i++)
+				Runtime.getRuntime().gc();
 
 			int ite = 0;
 			for (; ite < iteracion; ite++) {
+				
+				for (int i = 0; i < 5; i++)
+					Runtime.getRuntime().gc();
 
 				quick = new int[arraySize];
-
+				
 				for (int i = 0; i < arraySize; ++i) {
 					quick[i] = (int) (Math.random() * (arraySize));
 				}
@@ -120,30 +142,61 @@ public class Main {
 					// System.out.print(Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory());
 				}
 				mershe = quick.clone();
-
-				memoriaQuick += (Runtime.getRuntime().freeMemory());
-				quicktime -= System.nanoTime();
+				
+				for (int i = 0; i < 5; i++)
+					Runtime.getRuntime().gc();
+				
+				//Quicksort
+				//Initialize locals
+				long memoriaQuick = (Runtime.getRuntime().freeMemory());
+				double quickTime = 0 - System.nanoTime();
 				quicksort(quick, 0, quick.length - 1);
-				quicktime += System.nanoTime();
+				quickTime += System.nanoTime();
 				memoriaQuick -= (Runtime.getRuntime().freeMemory());
+				//Add to totals
+				quickTimeTotal += quickTime;
+				memoriaQuickTotal += memoriaQuick;
+				//Add to CSV
+				fileWriter.append(Long.toString(memoriaQuick));
+				fileWriter.append(',');
+				fileWriter.append(Double.toString(quickTime));
+				fileWriter.append(',');
 
+				
+				for (int i = 0; i < 5; i++)
+					Runtime.getRuntime().gc();
+			 
+				//Mergesort
+				//Initialize locals
+				double mergeTime = 0 -System.nanoTime();
+				long mergeMemory = mergesort(mershe, 0, mershe.length - 1);
+				mergeTime += System.nanoTime();
+				//Add to totals
+				mergeTimeTotal += mergeTime;
+				memoriaMergeTotal += mergeMemory;
+				//Add to CSV
+				fileWriter.append(Long.toString(mergeMemory));
+				fileWriter.append(',');
+				fileWriter.append(Double.toString(mergeTime));
+				fileWriter.append(',');
+						
+				
 				for (int i = 0; i < 5; i++)
 					Runtime.getRuntime().gc();
 
-				mershtime = -System.nanoTime();
-				memoriaMersh += mergesort(mershe, 0, mershe.length - 1);
-				mershtime += System.nanoTime();
-
-				for (int i = 0; i < 5; i++)
-					Runtime.getRuntime().gc();
-
+				//Binary Search
 				int aBuscar = (int) (Math.random() * (arraySize));
-
-				Bintime -= System.nanoTime();
+				double binTime = 0 - System.nanoTime();
 				BusBin(quick, aBuscar);
-				Bintime += System.nanoTime();
+				binTime += System.nanoTime();
+				//Add to total
+				binTimeTotal += binTime;
+				//Append to CSV
+				fileWriter.append(Double.toString(binTime));
+				fileWriter.append(',');
 
-				Sectime -= System.nanoTime();
+				//Sequential Search
+				double secTime = 0 - System.nanoTime();
 				boolean aux = true;
 				int i = 0;
 				while (aux == true && i < quick.length) {
@@ -152,29 +205,49 @@ public class Main {
 					}
 					i++;
 				}
-				Sectime += System.nanoTime();
-
+				secTime += System.nanoTime();
+				//Add to total
+				secTimeTotal += secTime;
+				//Append to CSV
+				fileWriter.append(Double.toString(secTime));
+				fileWriter.append(',');
+				
 				for (int j = 0; j < 5; j++)
 					Runtime.getRuntime().gc();
+				
+				//End the line for the CSV
+				fileWriter.append('\n');
 			}
+			//Print means to CSV
+			int itNum = (arraySize-5000)/2500;
+			char[] columns = {'A','B','C','D','E','F'};
+			fileWriter.append("Totales:\n");
+			for(char c :columns) fileWriter.append("=SUM("+ c + (3+ 5*itNum + iteracion*itNum) + 
+												":" + c + (2+ 5*itNum + iteracion  +iteracion*itNum)+ ")/" + ite +",");	
+			fileWriter.append('\n');
+			fileWriter.append('\n');
 
-			quicktime /= ite;
-			mershtime /= ite;
-			Bintime /= ite;
-			Sectime /= ite;
-			memoriaQuick /= ite;
-			memoriaMersh /= ite;
+			quickTimeTotal /= ite;
+			mergeTimeTotal /= ite;
+			binTimeTotal /= ite;
+			secTimeTotal /= ite;
+			memoriaQuickTotal /= ite;
+			memoriaMergeTotal /= ite;
 
 			System.out.println("Para tamaÃ±o " + arraySize + ":");
-			System.out.println("\tQUICK Memoria " + memoriaQuick);
-			System.out.println("\tMERSH Memoria " + memoriaMersh);
-			System.out.println("\tQUICK " + quicktime + " nsec");
-			System.out.println("\tMERSH " + mershtime + " nsec");
-			System.out.println("\tBINARY " + Bintime + " nsec");
-			System.out.println("\tSecuencial " + Sectime + " nsec");
+			System.out.println("\tQUICK Memoria " + memoriaQuickTotal);
+			System.out.println("\tMERSH Memoria " + memoriaMergeTotal);
+			System.out.println("\tQUICK " + quickTimeTotal + " nsec");
+			System.out.println("\tMERSH " + mergeTimeTotal + " nsec");
+			System.out.println("\tBINARY " + binTimeTotal + " nsec");
+			System.out.println("\tSecuencial " + secTimeTotal + " nsec");
 			for (int i = 0; i < 5; i++)
 				Runtime.getRuntime().gc();
 		}
+		
+        fileWriter.flush();
+        fileWriter.close();
+
 
 	}
 }
